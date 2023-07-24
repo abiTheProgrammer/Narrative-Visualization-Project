c62351b8e5d3461d60818c25798c5ef3069dde77
Abhinav Mahesh <amahesh6@illinois.edu>
2023-07-24 16:26:58 -0500

    Scene 2 completed

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-function scene1() {
-    // Use D3 to select the div with id "myDiv"
-    const div = d3.select("#myDiv");
+d3.csv("playlist.csv").then(data => {
+    let sceneIndex = 0; // Initialize the scene index
+    const div = d3.select(".scene"); // Use D3 to select the div with class "scene"
 
-    // Append an h1 heading to the selected div and set text and CSS class
-    div.append("h1")
-    .text("Top Songs")
-    .attr("class", "heading");
+    // Use D3 to select the buttons
+    const prevBtn = d3.select(".prevBtn");
+    const nextBtn = d3.select(".nextBtn");
 
-    // Append a paragraph to the selected div and set text and CSS class
-    div.append("p")
-    .html("Overview of songs that have appeared most frequently on the <b>Spotify Top 50 Playlist</b>.")
-    .attr("class", "paragraph");
+    // Function to update button visibility based on the scene
+    const updateButtonVisibility = (sceneIndex) => {
+        if (sceneIndex === 0) {
+            prevBtn.style("display", "none");
+        } else if (sceneIndex === 2) {
+            nextBtn.style("display", "none");
+        } else {
+            prevBtn.style("display", "block");
+            nextBtn.style("display", "block");
+        }
+    };
 
-    // Load the data from the CSV file
-    d3.csv("playlist.csv").then(data => {
+    const createScene1 = () => {
+        
+        // Append an h1 heading to the selected div and set text and CSS class
+        div.append("h1")
+        .text("Top Songs")
+        .attr("class", "heading");
+
+        // Append a paragraph to the selected div and set text and CSS class
+        div.append("p")
+        .html("Overview of songs that have appeared most frequently on the <b>Spotify Top 50 Playlist</b>.")
+        .attr("class", "paragraph");
+
         // Sort the data based on "position_in_playlist" in ascending order
         const filteredData = data.sort((a, b) => a.position_in_playlist - b.position_in_playlist);
 
@@ -24,7 +40,7 @@ function scene1() {
             count: entries.length
         }));
 
-        // Sort the grouped data based on the count of #1 positions in descending order
+        // Sort the grouped data based on the count of position_in_playlist in descending order
         const topSongs = groupedData.sort((a, b) => b.count - a.count);
 
         // Append a table to the selected div and create table rows and cells
@@ -34,7 +50,7 @@ function scene1() {
         const headerRow = table.append("tr");
         headerRow.append("th").text("Songs");
         headerRow.append("th").text("Artists");
-        headerRow.append("th").text("Frequency");
+        headerRow.append("th").text("Times Appeared");
 
         // Create table rows for each top song
         const rows = table.selectAll("tr")
@@ -42,11 +58,100 @@ function scene1() {
         .enter()
         .append("tr");
 
-        // Add cells for track name, artists, and count of #1 positions
+        // Add cells for track name, artists, and number of times appeared
         rows.append("td").text(d => d.track_name);
         rows.append("td").text(d => d.artists.replace(/[\[\]']/g, "").replace(/,/g, ", "));
         rows.append("td").text(d => d.count);
-    });
-}
+    };
 
-scene1();
+    const createScene2 = () => {
+
+       // Append an h1 heading to the selected div and set text and CSS class
+       div.append("h1")
+       .text("Top Streamed Songs")
+       .attr("class", "heading");
+
+       // Append a paragraph to the selected div and set text and CSS class
+       div.append("p")
+       .html("Highlighting the top-streamed songs and their artists based on their <b>Average Track Popularity Score.</b>")
+       .attr("class", "paragraph");
+
+        // Group the data by track_name and calculate the average track_popularity for each song
+        const groupedData = Array.from(d3.group(data, d => d.track_name), ([track_name, entries]) => {
+            const sumTrackPopularity = d3.sum(entries, d => d.track_popularity);
+            const averageTrackPopularity = parseInt(sumTrackPopularity / entries.length);
+            return { track_name, artists: entries[0].name_of_artists, average_popularity: averageTrackPopularity };
+        });
+        
+        // Sort the grouped data in descending order based on average_popularity >= 1
+        const sortedData = groupedData.sort((a, b) => b.average_popularity - a.average_popularity)
+        .filter(d => d.average_popularity >= 1);
+
+        // Append a table to the selected div and create table rows and cells
+        const table = div.append("table");
+
+        // Create table header row
+        const headerRow = table.append("tr");
+        headerRow.append("th").text("Songs");
+        headerRow.append("th").text("Artists");
+        headerRow.append("th").text("Average Track Popularity Score");
+
+        // Create table rows for each top song
+        const rows = table.selectAll("tr")
+        .data(sortedData)
+        .enter()
+        .append("tr");
+
+        // Add cells for track name, artists, and count of #1 positions
+        rows.append("td").text(d => d.track_name);
+        rows.append("td").text(d => d.artists.replace(/[\[\]']/g, "").replace(/,/g, ", "));
+        rows.append("td").text(d => d.average_popularity);
+    };
+    
+    const createScene3 = () => {
+      // Implement code for Scene 3 here
+    };
+    
+    // Initialize the narrative visualization with the first scene
+    createScene1();
+    updateButtonVisibility(sceneIndex);
+
+    // Add event listeners for navigation buttons
+
+    prevBtn.on("click", () => {
+        if (sceneIndex > 0) {
+            sceneIndex--;
+            updateButtonVisibility(sceneIndex);
+            updateScene();
+        }
+    });
+
+    nextBtn.on("click", () => {
+        if (sceneIndex < 2) {
+            sceneIndex++;
+            updateButtonVisibility(sceneIndex);
+            updateScene();
+        }
+    });
+
+    // Function to update the scene based on the current scene index
+    const updateScene = () => {
+      div.selectAll("*").remove(); // Clear the existing content
+
+      // Call the appropriate scene function based on the scene index
+      switch (sceneIndex) {
+        case 0:
+          createScene1();
+          break;
+        case 1:
+          createScene2();
+          break;
+        case 2:
+          createScene3();
+          break;
+        default:
+          break;
+      }
+    };
+
+});
